c124e0713cf85b992abc9cffd29d94d4812c9bce
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-09-19 16:37:19 -0500

    integrated openFDA api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
+import axios from "axios";
+
+function test(response) {
+ console.log(response);
+}
+
+function logSubmit(event) {
+ event.preventDefault();
+ let key = "6JlKzLqCMFly6SbLMcjq9ylzhrXC9Ltf29PqqPhe";
+ let url = `https://api.fda.gov/drug/event.json?api_key=${key}&search=(receivedate:[20040101+TO+20230919])+AND+lovastatin&count=receivedate`;
+ axios.get(url).then(test);
+}
+
 function App() {
- return <div className="App">hello</div>;
+ return (
+  <div className="App">
+   <form onSubmit={logSubmit}>
+    <input type="text" />
+    <input type="submit" value="Search" />
+   </form>
+  </div>
+ );
 }
 
 export default App;
