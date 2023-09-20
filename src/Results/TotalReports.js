26a7576fba30d192722022c5b67d4eb75360a565
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-09-20 14:04:51 -0500

    created component for total number of reports

---
+++ src/Results/TotalReports.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function TotalReports(props) {
+ return (
+  <div className="TotalReports">Total number of reports: {props.total}</div>
+ );
+}
--- src/Search.js
+++ src/Search.js
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import TotalReports from "./Results/TotalReports";
+
 export default function Search() {
+ let key = "6JlKzLqCMFly6SbLMcjq9ylzhrXC9Ltf29PqqPhe";
  let [query, setQuery] = useState("");
+ let [totalReports, setTotalReports] = useState("");
 
  function handleChange(event) {
   setQuery(event.target.value);
  }
 
- function logSubmit(event) {
+ function getResults(event) {
   event.preventDefault();
-  let key = "6JlKzLqCMFly6SbLMcjq9ylzhrXC9Ltf29PqqPhe";
-  let url = `https://api.fda.gov/drug/event.json?api_key=${key}&search=(receivedate:[20040101+TO+20230919])+AND+${query}&count=receivedate`;
-  axios.get(url).then(test);
+
+  let totalReportsUrl = `https://api.fda.gov/drug/event.json?api_key=${key}&search=(receivedate:[20040101+TO+20230919])+AND+${query}&count=receivedate`;
+  axios.get(totalReportsUrl).then(displayResults);
  }
 
- function test(response) {
-  console.log(response.data.results);
+ function displayResults(response) {
+  setTotalReports(response.data.results.length);
  }
 
  return (
-  <form onSubmit={logSubmit}>
-   <input type="text" onChange={handleChange} />
-   <input type="submit" value="Search" />
-  </form>
+  <div className="Search">
+   <form onSubmit={getResults}>
+    <input type="text" onChange={handleChange} />
+    <input type="submit" value="Search" />
+   </form>
+   <TotalReports total={totalReports} />
+  </div>
  );
 }
