52e2bc3eafffb468f72f633ff2a83822c0cb41d3
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-11-08 15:38:58 -0600

    fixed bug of drug name not showing

--- src/Results/TotalReports.js
+++ src/Results/TotalReports.js
@@ -5,9 +5,11 @@ export default function TotalReports(props) {
   return (
    <div className="TotalReports">
     <h2>
-     {" "}
-     <span className="capitalize">{props.drugName}</span>{" "}
-     <small>- Total number of reports: {props.totalReportsData}</small>
+     <small>
+      {" "}
+      <span className="capitalize">{props.drugName}</span> - Total number of
+      reports: {props.totalReportsData}
+     </small>
     </h2>
    </div>
   );
